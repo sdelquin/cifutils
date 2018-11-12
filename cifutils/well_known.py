9ea2b68c2cdd9f5bf4a3807db1177b3abd6fb21a
Sergio Delgado Quintero <sdelquin@gmail.com>
2018-11-12 16:31:13 +0000

    Fix well-known management

--- cifutils/well_known.py
+++ cifutils/well_known.py
@@ -6,7 +6,6 @@ from django.http import HttpResponse
 def acme_challenge(request, filename):
     path = os.path.join(
         settings.BASE_DIR,
-        "..",
         ".well-known",
         "acme-challenge",
         filename
