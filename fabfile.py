cf1f8fe53cd7e1c67d8d0397dea9822e557fc20f
Sergio Delgado Quintero <sdelquin@gmail.com>
2020-04-11 13:46:08 +0100

    Fix deploy script

--- fabfile.py
+++ fabfile.py
@@ -6,7 +6,7 @@ env.hosts = ['cloud']
 def deploy():
     local('git push')
     with prefix('source ~/.virtualenvs/cifutils/bin/activate'):
-        with cd('~/cifutils'):
+        with cd('~/code/cifutils'):
             run('git pull')
             run('pip install -r requirements.txt')
             run('python manage.py collectstatic --noinput')
