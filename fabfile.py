68a441e49ee17be0002ebb7583f426e3cdf8e195
Sergio Delgado Quintero <sdelquin@gmail.com>
2020-04-11 13:44:12 +0100

    Migrate from pipenv to requirements

--- fabfile.py
+++ fabfile.py
@@ -1,12 +1,13 @@
-from fabric.api import env, local, cd, run
+from fabric.api import env, local, cd, run, prefix
 
 env.hosts = ['cloud']
 
 
 def deploy():
     local('git push')
-    with cd('~/cifutils'):
-        run('git pull')
-        run('pipenv install')
-        run('pipenv run python manage.py collectstatic --noinput')
-        run('supervisorctl restart cifutils')
+    with prefix('source ~/.virtualenvs/cifutils/bin/activate'):
+        with cd('~/cifutils'):
+            run('git pull')
+            run('pip install -r requirements.txt')
+            run('python manage.py collectstatic --noinput')
+            run('supervisorctl restart cifutils')
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd $(dirname $0)
-source $(pipenv --venv)/bin/activate
-exec uwsgi --ini uwsgi.ini
+source ~/.virtualenvs/ciap/bin/activate
+cd "$(dirname "$0")"
+exec gunicorn -c gunicorn.conf.py cifutils.wsgi:application
