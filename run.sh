7468cf7b4e65c15f6848ecffacbc61f80b5ec55c
Sergio Delgado Quintero <sdelquin@gmail.com>
2022-12-08 11:36:07 +0000

    Fix master script

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-source ~/.virtualenvs/cifutils/bin/activate
+source ~/.pyenv/versions/cifutils/bin/activate
 cd "$(dirname "$0")"
 exec gunicorn -c gunicorn.conf.py cifutils.wsgi:application
